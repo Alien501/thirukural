81ba361fda06e2835c2a78dc98a5beca230b2651
Alien501 <cvignesh404@gmail.com>
2024-06-26 19:54:13 +0530

    bug fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '^/api': {
-        // target: 'https://thirukural-six.vercel.app',
-        target: 'http://localhost:3000',
+        target: 'https://thirukural-six.vercel.app',
+        // target: 'http://localhost:3000',
         changeOrigin: true
       }
     }
