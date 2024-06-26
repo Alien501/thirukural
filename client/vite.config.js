527a1a48507fee951bfeaa2001a952f4554c5a17
Alien501 <cvignesh404@gmail.com>
2024-06-26 19:56:43 +0530

    This should work

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
     proxy: {
       '^/api': {
         target: 'https://thirukural-six.vercel.app',
-        // target: 'http://localhost:3000',
         changeOrigin: true
       }
     }
