ad95f2be68af31e3db0648003949e2a5c042c664
Alien501 <cvignesh404@gmail.com>
2024-06-26 20:48:20 +0530

    This should

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '^/api': {
-        target: 'https://thirukural-six.vercel.app',
+        target: 'https://thirukural-six.vercel.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
